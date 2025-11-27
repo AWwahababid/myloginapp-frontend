bd7f9df2d1d088a976dce206755ebd76d23ff91c
AWwahababid <abidwahab34@gmail.com>
2025-11-27 15:12:24 +0500

    deploy

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  //baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: "https://myloginapp-backend.vercel.app/api", // backend URL LIVE VERCEL
 });
 
 // Add token to headers if available
